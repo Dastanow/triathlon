4a605491e2883b5072b3c3b38143921a5104af7d
Nursultan <amanturovnursultan458@gmail.com>
2024-03-17 22:05:57 +0600

    add logig in component vacancies

--- src/Sections/Vacancies/index.jsx
+++ src/Sections/Vacancies/index.jsx
@@ -1,11 +1,27 @@
-import YesVacancies from './YesVacancies/YesVacancies'
+import  { useEffect, useState } from 'react';
+import YesVacancies from './YesVacancies/YesVacancies';
+import NoVacancies from './NoVacancies/NoVacancies'
+import vacanciesData from './YesVacancies/VacanciesFakeData.json'; // Путь к JSON файлу
 
 const IndexJSX = () => {
+    const [hasVacancies, setHasVacancies] = useState(false);
+
+    useEffect(() => {
+        // Проверяем, есть ли массив с объектами в данных
+        const checkForVacancies = () => {
+            if (Array.isArray(vacanciesData) && vacanciesData.length > 0) {
+                setHasVacancies(true);
+            }
+        };
+
+        checkForVacancies();
+    }, []);
+
     return (
         <div>
-            <YesVacancies/>
+            {hasVacancies ? <YesVacancies vacanciesData={vacanciesData} /> : <NoVacancies/>}
         </div>
-    )
-}
+    );
+};
 
-export default IndexJSX
+export default IndexJSX;

346805c606d68b0e8a84f5903290e41fc5c3c64c
dastanasetullaev05 <legendaloukik@gmail.com>
2024-03-21 01:01:24 +0600

    made routing

---
+++ src/providers/RouterProvider.jsx
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom'
+import Layout from './Layout'
+import FirstPage from '../Pages/FisrtPage/FirstPage'
+import IndexJSX from '../Sections/Vacancies'
+import PageNotFound from '../Sections/Error404/PageNotFound'
+
+const Routing = () => {
+    return (
+        <div className="routing--first__container">
+            <Routes>
+                <Route path="/" element={<Layout/>}>
+                    <Route index element={<FirstPage/>}/>
+                    <Route path="/Vacancies" element={<IndexJSX />} />
+                </Route>
+                <Route path="*" element={<PageNotFound/>} />
+            </Routes>
+        </div>
+    )
+}
+
+export default Routing

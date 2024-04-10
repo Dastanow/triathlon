a37b2ad27f06b3240adfcf3e1a2964f09b49a5b8
Nursultan <amanturovnursultan458@gmail.com>
2024-04-10 11:08:57 +0600

    dff

--- src/Sections/HeroBlock/HeroBlock.jsx
+++ src/Sections/HeroBlock/HeroBlock.jsx
@@ -7,8 +7,6 @@ import { useState } from 'react';
 
 const HeroBlock = () => {
     const [modalActive, setModalActive] = useState(false)
-<<<<<<< HEAD
-=======
     const ScrollToTop = () => {  
         window.scrollTo({
             top: 0,
@@ -17,7 +15,6 @@ const HeroBlock = () => {
     };
     
     
->>>>>>> 84a4b7f0ee0e016dad491a2a5eb90a721773e200
     return (
         <div className="main_unit_home">
             <div className="block">
@@ -26,34 +23,15 @@ const HeroBlock = () => {
 
                     <p className="text">
                     Первая инклюзивная спортивная база в Кыргызской Республике
-<<<<<<< HEAD
                 </p>
-                <div className="menu_btn">
-                    <button  onClick={() => setModalActive(true)} className="btn">Оставить заявку</button>
-                </div>
-=======
-                    </p>
 
                     <div className="menu_btn">
                         <button  onClick={() => setModalActive(true)} className="btn">Оставить заявку</button>
                     </div>
->>>>>>> 84a4b7f0ee0e016dad491a2a5eb90a721773e200
 
                     <ModaleWindow active={modalActive} setActive={setModalActive}/> 
 
-<<<<<<< HEAD
                
-=======
-                    <Link to="/" onClick={ScrollToTop}>
-                        <img className="theTop" src={arrow} alt="" />
-                    </Link>
-
-                    <div className="whatapp">
-                        <a target="_blank" href="https://api.whatsapp.com/send/?phone=996227000180&type=phone_number&app_absent=0&fbclid=PAAaY_5bq244tz8NBMZByR5gZH5QOPPnwqjB2n58ehJl3PUjGfA57gTD3vBKw_aem_AT59MfZLMHLdpFec1QkPFlU_LqH-cBOTMTSLfwbadUyszfb7PcECLptkeZoEOPpWcHE">
-                            <img src={wp} alt="" />
-                        </a>
-                    </div>
->>>>>>> 84a4b7f0ee0e016dad491a2a5eb90a721773e200
 
                 </div>
 
@@ -63,7 +41,3 @@ const HeroBlock = () => {
 };
 
 export default HeroBlock;
-<<<<<<< HEAD
-
-=======
->>>>>>> 84a4b7f0ee0e016dad491a2a5eb90a721773e200

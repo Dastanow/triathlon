06b98bba2517f3f5d5ce76a85cae494a1dafdd1d
unknown <kaldybaevsultan518@gmail.com>
2024-04-14 15:39:15 +0300

    fixed-adaptive

--- src/Sections/Home/Home.jsx
+++ src/Sections/Home/Home.jsx
@@ -1,7 +1,7 @@
 // import React from 'react';
 import './Home.scss';
 import wp from '../../Assets/wp1.png';
-import ModaleWindow from '../../Modules/ModaleWindow';
+import ModalWindow from '../../Modules/ModaleWindow';
 import { useState } from 'react';
 
 
@@ -23,7 +23,7 @@ const Home = () => {
                         <button  onClick={() => setModalActive(true)} className="btn">Оставить заявку</button>
                     </div>
 
-                    <ModaleWindow active={modalActive} setActive={setModalActive}/>  
+                    <ModalWindow active={modalActive} setActive={setModalActive}/>  
 
                     <div className="whatapp">
                         <a href="https://api.whatsapp.com/send/?phone=996227000180&type=phone_number&app_absent=0&fbclid=PAAaY_5bq244tz8NBMZByR5gZH5QOPPnwqjB2n58ehJl3PUjGfA57gTD3vBKw_aem_AT59MfZLMHLdpFec1QkPFlU_LqH-cBOTMTSLfwbadUyszfb7PcECLptkeZoEOPpWcHE">

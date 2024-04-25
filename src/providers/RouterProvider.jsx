1e55211527d53708646ff905ed6c7c808a95faf2
azam2802 <azamumarov182@gmal.com>
2024-04-26 00:16:38 +0600

    fix: merge dev

--- src/providers/RouterProvider.jsx
+++ src/providers/RouterProvider.jsx
@@ -1,17 +1,23 @@
 import { Route, Routes } from 'react-router-dom'
 import Layout from './Layout'
-import { HomePage, VacanciesPage, NotFoundPage, AdminPage, SchedulePage } from '@pages';
+import {
+    HomePage,
+    VacanciesPage,
+    NotFoundPage,
+    AdminPage,
+    SchedulePage,
+} from '@pages'
 
 const Routing = () => {
     return (
         <Routes>
-            <Route path="/" element={<Layout/>}>
-                <Route index element={<HomePage/>}/>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<HomePage />} />
                 <Route path="vacancies" element={<VacanciesPage />} />
             </Route>
-            <Route path="schedule" element={<SchedulePage/>} />
-            <Route path="personal-account" element={<AdminPage/>} />
-            <Route path="*" element={<NotFoundPage/>} />
+            <Route path="schedule" element={<SchedulePage />} />
+            <Route path="personal-account" element={<AdminPage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Routes>
     )
 }

b6334fbe976d93c30ee2b1682f84d970b84dbffe
azam2802 <azamumarov182@gmal.com>
2024-04-25 21:30:47 +0600

    fix: vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
             '@assets': path.resolve(__dirname, './src/Assets'),
             '@common': path.resolve(__dirname, './src/common'),
             '@components': path.resolve(__dirname, './src/Components'),
-            '@hooks': path.resolve(__dirname, './src/Hooks'),
+            '@hooks': path.resolve(__dirname, './src/hooks'),
             '@modules': path.resolve(__dirname, './src/Modules'),
             '@pages': path.resolve(__dirname, './src/Pages'),
             '@providers': path.resolve(__dirname, './src/providers'),

c9aea54c0783803ecd44704fc6181cbb782dbf21
azam2802 <azamumarov182@gmal.com>
2024-05-03 20:11:19 +0600

    remove: api folder in App

--- src/App/index.js
+++ src/App/index.js
@@ -1,4 +1,1 @@
-export { trainAreas, fetchAll as fetchAllTrains } from './api/trainAreas.api'
-export { coaches, fetchAll as fetchAllCoaches } from './api/coaches.api'
-export { comments, fetchAll as fetchAllComments } from './api/comments.api'
 export { axiosAPI } from './interceptionLang/interceptionLang'

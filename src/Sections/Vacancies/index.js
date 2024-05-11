125bdc64dfb6d516d84e556bfb3cd95a652e7a61
azam2802 <azamumarov182@gmal.com>
2024-05-11 21:42:53 +0600

    hotfix

--- src/Sections/Vacancies/index.js
+++ src/Sections/Vacancies/index.js
@@ -1,4 +1,2 @@
-import data from './Vacancies/VacanciesFakeData.json';
 export { Vacancies } from './Vacancies/Vacancies';
 export { EmptyBlock } from './EmptyBlock/EmptyBlock';
-export { data as VacanciesData }

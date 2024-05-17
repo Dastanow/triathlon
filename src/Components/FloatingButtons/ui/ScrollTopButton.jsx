3e8b75a19c58f884d07395e629dd18aa1d0da9a4
azam2802 <azamumarov182@gmal.com>
2024-05-17 22:19:25 +0600

    add: scroll to top on location change

--- src/Components/FloatingButtons/ui/ScrollTopButton.jsx
+++ src/Components/FloatingButtons/ui/ScrollTopButton.jsx
@@ -1,12 +1,24 @@
 import { useEffect, useState, useRef } from 'react'
 import '../FloatingButtons.scss'
 import { FaChevronUp } from 'react-icons/fa'
+import { useLocation } from 'react-router-dom'
 
 export const ScrollTopButton = () => {
+    const location = useLocation()
     const [showScroll, setShowScroll] = useState(false)
     const [isEndOfHeroBlock, setIsEndOfHeroBlock] = useState(false)
 
     const scrollToTopRef = useRef(null)
+
+    useEffect(() => {
+        let currentPosition = window.scrollY
+        if (currentPosition > 0) {
+            window.scrollTo({
+                top: 0,
+                behavior: 'instant',
+            })
+        }
+    }, [location.pathname])
 
     useEffect(() => {
         const handleScroll = () => {

bcf29671ddbb23922113addceaf92f51fae89217
dastanasetullaev05 <legendaloukik@gmail.com>
2024-05-20 23:03:05 +0600

    fix: Hero block made api

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -1,29 +1,55 @@
-import { useState } from 'react'
-import { CustomButton } from '@ui'
-import { Container } from '@components'
-import './Hero.scss'
-import { useTranslation } from 'react-i18next'
-import CustomModal from '@/UI/CustomModal/CustomModal'
+import { useEffect, useState } from 'react';
+import { CustomButton } from '@ui';
+import { Container } from '@components';
+import './Hero.scss';
+import { useTranslation } from 'react-i18next';
+import CustomModal from '@/UI/CustomModal/CustomModal';
+import { axiosAPI } from '@/App';
 
 export const Hero = () => {
-    const [modalActive, setModalActive] = useState(false)
+    const [modalActive, setModalActive] = useState(false);
+    const [heroApi, setHeroApi] = useState();
 
-    const { t } = useTranslation()
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data } = await axiosAPI.get('homepage/');
+                setHeroApi(data);
+            } catch (error) {
+                console.log('error');
+            }
+        };
+        fetchData();
+    }, []);
+
+    const { t } = useTranslation();
     return (
         <section className="hero" id="hero">
+            {heroApi?.map((item) => {
+                return (
+                    <video className="heroVideo" autoPlay loop muted>
+                     <source src={item.photo} type="video/mp4" />
+                    </video>
+                )
+            }) 
+            }
             <Container classNames="heroContainer">
-                <div className="heroContent">
-                    <h1 className="heroContentTitle">TRIATHLON CENTER</h1>
-                    <p className="heroContentDescription">
-                        Первая инклюзивная спортивная база в Кыргызской
-                        Республике
-                    </p>
-                    <CustomButton
-                        type="secondary"
-                        onClick={() => setModalActive(true)}>
-                        {t('buttonHero')}
-                    </CustomButton>
-                </div>
+                {heroApi?.map((item) => {
+                    return (
+                        <div className="heroContent">
+                        <h1 className="heroContentTitle">{item.title}</h1>
+                        <p className="heroContentDescription">
+                            {item.subtitle}
+                        </p>
+                        <CustomButton
+                            type="secondary"
+                            onClick={() => setModalActive(true)}>
+                            {t('buttonHero')}
+                        </CustomButton>
+                    </div>
+                    )
+                })     
+                }
             </Container>
             <CustomModal
                 type="leaveRequest"
@@ -32,5 +58,5 @@ export const Hero = () => {
                 isOpen={modalActive}
             />
         </section>
-    )
-}
+    );
+};

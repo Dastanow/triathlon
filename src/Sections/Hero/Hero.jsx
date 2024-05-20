5b58b60f48e76fa6705b9269a319a5139969378e
Dayan <dayanulanov.wer@gmail.com>
2024-05-21 04:56:17 +0600

    fix: white text and hero block

--- src/Sections/Hero/Hero.jsx
+++ src/Sections/Hero/Hero.jsx
@@ -1,36 +1,58 @@
-import { useState } from 'react'
-import { CustomButton } from '@ui'
-import { Container } from '@components'
-import './Hero.scss'
-import { useTranslation } from 'react-i18next'
-import CustomModal from '@/UI/CustomModal/CustomModal'
+import { useEffect, useState } from 'react';
+import { CustomButton } from '@ui';
+import { Container } from '@components';
+import './Hero.scss';
+import { useTranslation } from 'react-i18next';
+import CustomModal from '@/UI/CustomModal/CustomModal';
+import { axiosAPI } from '@/App';
 
 export const Hero = () => {
-    const [modalActive, setModalActive] = useState(false)
+    const [modalActive, setModalActive] = useState(false);
+    const [heroData, setHeroData] = useState([]);
+    const { i18n } = useTranslation();
 
-    const { t } = useTranslation()
+    const fetchData = async () => {
+        try {
+            const { data } = await axiosAPI.get('/homepage/');
+            setHeroData(data);
+        } catch (error) {
+            console.error('Error fetching data:', error);
+        }
+    };
+
+    console.log(heroData);
+
+    useEffect(() => {
+        fetchData();
+    }, [i18n.language]);
+
+    const { t } = useTranslation();
+
     return (
-        <section className="hero" id="hero">
-            <Container classNames="heroContainer">
-                <div className="heroContent">
-                    <h1 className="heroContentTitle">TRIATHLON CENTER</h1>
-                    <p className="heroContentDescription">
-                        Первая инклюзивная спортивная база в Кыргызской
-                        Республике
-                    </p>
-                    <CustomButton
-                        type="secondary"
-                        onClick={() => setModalActive(true)}>
-                        {t('buttonHero')}
-                    </CustomButton>
-                </div>
-            </Container>
-            <CustomModal
-                type="leaveRequest"
-                title={t('buttonHero')}
-                close={() => setModalActive(false)}
-                isOpen={modalActive}
-            />
-        </section>
-    )
-}
+        <>
+            {heroData.map((item, index) => (
+                <section key={index} className="hero" id="hero"
+                    style={{ backgroundImage: `url(${item.photo}) no-repeat 0 50%`, backgroundSize: 'cover', }}
+                >
+                    <Container classNames="heroContainer">
+                        <div className="heroContent">
+                            <h1 className="heroContentTitle">{item.title}</h1>
+                            <p className="heroContentDescription">{item.subtitle}</p>
+                            <CustomButton
+                                type="secondary"
+                                onClick={() => setModalActive(true)}>
+                                {t('buttonHero')}
+                            </CustomButton>
+                        </div>
+                    </Container>
+                    <CustomModal
+                        type="leaveRequest"
+                        title={t('buttonHero')}
+                        close={() => setModalActive(false)}
+                        isOpen={modalActive}
+                    />
+                </section>
+            ))}
+        </>
+    );
+};

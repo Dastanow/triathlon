3b8ea0473114bbe61644135823cfb41bfec534ee
azam2802 <azamumarov182@gmal.com>
2024-06-04 22:51:20 +0600

    Input phone mask fixed

--- src/Sections/FAQ/CustomInput.jsx
+++ src/Sections/FAQ/CustomInput.jsx
@@ -1,9 +1,21 @@
-import PropTypes from 'prop-types';
-import errorIcon from '@assets/dangerous.svg';
-import './CustomInput.scss';
+import PropTypes from 'prop-types'
+import errorIcon from '@assets/dangerous.svg'
+import './CustomInput.scss'
+import { useMask } from '@react-input/mask'
 
-const CustomInput = ({ type, onChange, value, placeholder, error, label, id }) => {
-
+const CustomInput = ({
+    type,
+    onChange,
+    value,
+    placeholder,
+    error,
+    label,
+    id,
+}) => {
+    const inputRef = useMask({
+        mask: '+996 (___) ___-___',
+        replacement: { _: /\d/ },
+    })
     return (
         <div className={`customLabelForm ${error ? 'errorInput' : ''}`}>
             {type === 'textarea' ? (
@@ -24,6 +36,14 @@ const CustomInput = ({ type, onChange, value, placeholder, error, label, id }) =
                     <span className="customCheckboxCheckmark" />
                     {label}
                 </label>
+            ) : type === 'phone' ? (
+                <input
+                    className="customInputForm"
+                    type="text"
+                    onChange={onChange}
+                    placeholder={placeholder}
+                    ref={inputRef}
+                />
             ) : (
                 <input
                     className="customInputForm"
@@ -40,17 +60,18 @@ const CustomInput = ({ type, onChange, value, placeholder, error, label, id }) =
                 </>
             )}
         </div>
-    );
-};
+    )
+}
 
 CustomInput.propTypes = {
-    type: PropTypes.oneOf(['default', 'email', 'phone', 'textarea', 'checkbox']).isRequired,
+    type: PropTypes.oneOf(['default', 'email', 'phone', 'textarea', 'checkbox'])
+        .isRequired,
     onChange: PropTypes.func.isRequired,
     value: PropTypes.oneOfType([PropTypes.string, PropTypes.bool]),
     placeholder: PropTypes.string,
     error: PropTypes.string,
     label: PropTypes.string,
-    id: PropTypes.string
-};
+    id: PropTypes.string,
+}
 
-export default CustomInput;
+export default CustomInput
